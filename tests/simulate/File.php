6c325d8c57527795a686066cf67570679aba2fa7
joomaio.tinhcv <cvtinh2000@gmail.com>
2022-12-27 18:41:55 +0700

    add simulate app

---
+++ tests/simulate/File.php
@@ -0,0 +1,19 @@
+<?php
+namespace unittest\simulate;
+
+use SPT\File as TestFile;
+
+class File extends TestFile
+{
+    public function upload(array $file)
+    {
+        if( ! $this->check($file) ) return false;
+ 
+        if ( !copy($file["tmp_name"], $this->targetFile)) {
+            $this->error = $newFileName. ': File can not upload, please check folder permission.';
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ tests/simulate/appTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * SPT software - Application
+ * 
+ * @project: https://github.com/smpleader/spt
+ * @author: Pham Minh - smpleader
+ * @description: Just a basic Application implement mvc
+ * 
+ */
+
+namespace unittest\simulate; 
+
+use App\libraries\appPlg; 
+
+class appTest extends appPlg
+{
+    public function redirect($url = null)
+    {
+        if (!$this->get('url_redirect', ''))
+        {
+            $this->set('url_redirect', $url);
+        }
+    }
+
+    public function response($content, $code='200')
+    {
+        $this->set('respone_content', $content);
+        $this->set('respone_code', $code);
+    }
+}
