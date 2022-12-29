a8e6f8ffb32fa89d8d83248014e531381d6eaad0
joomaio.tinhcv <cvtinh2000@gmail.com>
2022-12-29 10:24:41 +0700

    create simulate test

---
+++ tests/simulate/File.php
@@ -0,0 +1,23 @@
+<?php
+namespace Tests\simulate;
+
+use SPT\File as TestFile;
+
+class File extends TestFile
+{
+    public function upload(array $file)
+    {
+        if( ! $this->check($file) ) return false;
+ 
+        if ( !copy($file["tmp_name"], $this->targetFile)) {
+            $this->error = $newFileName. ': File can not upload, please check folder permission.';
+            return false;
+        }
+        else
+        {
+            unlink($this->targetFile);
+        }
+        
+        return true;
+    }
+}
